51ebc353c1f082741e4cbef788870e783e7706df
liangwd <liangwd@bosma.com.cn>
2022-04-07 13:47:01 +0800

    v2 done

--- promise-func-type-v2.js
+++ promise-func-type-v2.js
@@ -9,18 +9,52 @@ function EasyPromise(func) {
 
   func(_resolveHook, _rejectHook);
 
-  let status = PENDING;
-  let awaitQueue = [];
-  let value;
-  let reason;
+  let _status = PENDING;
+  let _awaitQueue = [];
+  let _value;
+  let _reason;
 
   this.then = function (onFulfilled, onRejected) {
     const promise2 = new EasyPromise(function (resolve, reject) {
-      if (status === PENDING) {
-      } else if (status === FULFILLED) {
-        const x = onFulfilled(value);
-      } else if (status === REJECTED) {
-        const x = onRejected(r);
+      if (_status === PENDING) {
+        _awaitQueue.push({
+          resolve: resolveFunc,
+          reject: rejectFunc,
+        });
+      } else if (_status === FULFILLED) {
+        resolveFunc(_value);
+      } else if (_status === REJECTED) {
+        rejectFunc(_reason);
+      }
+
+      function resolveFunc(v) {
+        setTimeout(function () {
+          if (typeof onFulfilled === "function") {
+            try {
+              const x = onFulfilled(v);
+              _resolvePromise(promise2, x, resolve, reject);
+            } catch (e) {
+              reject(e);
+            }
+          } else {
+            resolve(v);
+          }
+        }, 0);
+      }
+
+      function rejectFunc(r) {
+        setTimeout(function () {
+          if (typeof onRejected === "function") {
+            try {
+              const x = onRejected(r);
+              _resolvePromise(promise2, x, resolve, reject);
+            } catch (e) {
+              reject(e);
+            }
+          } else {
+            reject(r);
+          }
+        }, 0);
       }
     });
 
@@ -28,121 +62,59 @@ function EasyPromise(func) {
   };
 
   function _resolveHook(v) {
-    if (status === PENDING) {
-      value = v;
-      status = FULFILLED;
+    if (_status === PENDING) {
+      _value = v;
+      _status = FULFILLED;
 
-      awaitQueue.splice(0).forEach(function (args) {
-        _resolveOrReject(args);
+      _awaitQueue.splice(0).forEach(function ({ resolve }) {
+        resolve(v);
       });
     }
   }
 
   function _rejectHook(r) {
-    if (status === PENDING) {
-      reason = r;
-      status = REJECTED;
+    if (_status === PENDING) {
+      _reason = r;
+      _status = REJECTED;
 
-      awaitQueue.splice(0).forEach(function (args) {
-        _resolveOrReject(args);
+      _awaitQueue.splice(0).forEach(function ({ reject }) {
+        reject(r);
       });
     }
   }
 
-  function _resolveOrReject([
-    onFulfilled,
-    onRejected,
-    promise2,
-    resolve,
-    reject,
-  ]) {
-    const isFulfilled = status === FULFILLED;
-    const handler = isFulfilled ? onFulfilled : onRejected;
+  function _resolvePromise(promise2, x, resolve, reject) {
+    if (promise2 === x) {
+      reject(
+        new TypeError("The `promise` and `x` cannot refer to the same object.")
+      );
+    }
 
-    if (typeof handler === "function") {
-      setTimeout(function () {
-        try {
-          const x = handler(isFulfilled ? value : reason);
-          if (x === promise2) {
-            throw new TypeError(
-              "The `promise` and `x` cannot refer to the same object."
-            );
-          }
+    if (x && (typeof x === "object" || typeof x === "function")) {
+      let lock = false;
 
-          if (
-            typeof x === "function" ||
-            (x !== null && typeof x === "object")
-          ) {
-            const then = x.then;
+      try {
+        const then = x.then;
 
-            if (typeof then === "function") {
-              let xStatus = PENDING;
-              try {
-                then.apply(x, [
-                  function (y) {
-                    if (xStatus === PENDING) {
-                      xStatus = FULFILLED;
-                      innerResolver(y);
-
-                      function innerResolver(_y) {
-                        let __status = PENDING;
-                        try {
-                          const then = _y && _y.then;
-                          if (typeof then === "function") {
-                            then.apply(_y, [
-                              function (__v) {
-                                if (__status === PENDING) {
-                                  __status = FULFILLED;
-                                  innerResolver(__v);
-                                }
-                              },
-                              function (__r) {
-                                if (__status === PENDING) {
-                                  __status = REJECTED;
-                                  reject(__r);
-                                }
-                              },
-                            ]);
-                          } else {
-                            if (__status === PENDING) {
-                              __status = FULFILLED;
-                              resolve(_y);
-                            }
-                          }
-                        } catch (e) {
-                          if (__status === PENDING) {
-                            __status = REJECTED;
-                            reject(e);
-                          }
-                        }
-                      }
-                    }
-                  },
-                  function (r) {
-                    if (xStatus === PENDING) {
-                      xStatus = REJECTED;
-                      reject(r);
-                    }
-                  },
-                ]);
-              } catch (_e) {
-                if (xStatus === PENDING) {
-                  xStatus = REJECTED;
-                  reject(_e);
-                }
-              }
-            } else {
-              resolve(x);
+        if (typeof then === "function") {
+          then.call(
+            x,
+            function (y) {
+              !lock &&
+                ((lock = true), _resolvePromise(promise2, y, resolve, reject));
+            },
+            function (r) {
+              !lock && ((lock = true), reject(r));
             }
-          } else {
-            resolve(x);
-          }
-        } catch (e) {
-          reject(e);
+          );
+        } else {
+          !lock && ((lock = true), resolve(x));
         }
-      }, 0);
+      } catch (e) {
+        !lock && ((lock = true), reject(e));
+      }
     } else {
-      isFulfilled ? resolve(value) : reject(reason);
+      resolve(x);
     }
   }
 }
